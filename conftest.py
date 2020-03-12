925fdfef490c4b2df841a5027a7b80144775e772
jinder1s <msingh@edx.org>
2020-03-12 14:42:55 -0400

    fixing comment

--- conftest.py
+++ conftest.py
@@ -8,5 +8,7 @@ pp = pprint.PrettyPrinter(indent=4)
 def pytest_sessionfinish(session):
     fixture_store = get_fixture_store(session)
     results_bag = fixture_store['results_bag']
-    # results_bag contains any new info stored by tests, here I would take that info and store in a json file
+    # results_bag contains any new info stored by tests, 
+    # here I would parse through info in results bag and store it in either dict or dataframe
+    # and save to file
 
